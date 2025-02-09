bca33cd21ba3afeae6ed887422836956ff5e1e16
Marcos-AS <marcossantangelo1@gmail.com>
2025-02-09 18:46:02 -0300

    mongo configurado y andando, mas simple

--- src/main/java/com/example/pizzamania/Pizza.java
+++ src/main/java/com/example/pizzamania/Pizza.java
@@ -2,15 +2,6 @@ package com.example.pizzamania;
 
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
-
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
-import org.springframework.data.cassandra.core.mapping.Table;
-
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 
 import lombok.Data;
 // import jakarta.persistence.Entity;
@@ -24,25 +15,18 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Data
-@Table("pizzas")
 public class Pizza {
 
-    @PrimaryKeyColumn(type = PrimaryKeyType.PARTITIONED)
-    private UUID id = Uuids.timeBased();
-
-    
     @NotNull
     @Size(min=5, message="El nombre debe tener al menos 5 caracteres")
     private String name;
 
-    @PrimaryKeyColumn(type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
     private Date createdAt = new Date();
     
     @Size(min=1, message="Debes elegir al menos 1 ingrediente")
-    @Column("ingredients")
-    private List<IngredientUDT> ingredients;
+    private List<Ingredient> ingredients;
 
     public void addIngredient(Ingredient ingredient) {
-        this.ingredients.add(PizzaUDRUtils.toIngredientUDT(ingredient));
+        this.ingredients.add(ingredient);
     }
 }
